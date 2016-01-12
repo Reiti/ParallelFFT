32d9cc9f665012900c68366c8734e58db145dbc8
Michael Reitgruber <reitgruber.michael@gmail.com>
2016-01-12 01:39:28 +0100

    horrible mistakes

--- fft_rec.c
+++ fft_rec.c
@@ -10,46 +10,38 @@
 
 #define PI 3.14159265358979323846
 
-void fft(double complex *in, double complex *out, int len);
+void fft(double complex *in, double complex *out, int len, int offset, int step);
 
 int main(int argc, char *argv[])
 {
     //TODO Handle generator input and different size arrays
-
     double complex in[8] = {1,2,3,4,5,6,7,8};
     double complex out[8] = {0};
-    fft(in, out, 8);
+    fft(in, out, 8, 0, 1);
     for(int i = 0; i<8; i++) {
         printf("%f %+fi\n", creal(out[i]), cimag(out[i]));
     }
 
 }
 
-void fft(double complex *in, double complex *out, int len)
+
+void fft(double complex *in, double complex *out, int len, int offset, int step)
 {
-    double complex evens[len/2], odds[len/2];
-    double complex evensT[len/2], oddsT[len/2];
-
     if(len == 1) {
-        out[0] = in[0];
-        return;
+      out[0] = in[offset];
+      return;
     }
 
-    for(int i=0; i<len/2; i++) {
-        evens[i] = in[2*i];
-        odds[i] = in[2*i + 1];
-    }
+    double complex *evenT = out;
+    double complex *oddT = out+len/2;
+    fft(in, evenT, len/2, offset, step*2);
+    fft(in, oddT, len/2, offset+step, step*2);
 
-    fft(evens, evensT, len/2);
-    fft(odds, oddsT, len/2);
+    for(int i=0; i<len/2; i+=1) {
+      double complex twiddle = cexp(-2*PI*I*i/len) * oddT[i];
 
-    for(int i=0; i<len/2; i++) {
-        oddsT[i] = oddsT[i]*cexp(((-2)*PI*i*I)/len);
-    }
-
-    for(int i=0; i<len/2; i++) {
-        out[i] = evensT[i] + oddsT[i];
-        out[i + len/2] = evensT[i] - oddsT[i];
+      out[i] = evenT[i] + twiddle;
+      out[i + len/2] = evenT[i] -twiddle;
     }
 
 }
