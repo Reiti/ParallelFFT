a2196318df9c88257efc90fd863040931bcd53c2
wiede5335 <e1425647@student.tuwien.ac.at>
2016-01-19 11:23:26 +0100

    latest version... all the processors always work now

--- fft_mpi/fft_it_mpi.c
+++ fft_mpi/fft_it_mpi.c
@@ -27,6 +27,7 @@ void merge(int len);
 int ispow2(int len);
 	double complex * in;
 	double complex * out;
+int h=0;
 
 void frees(void){
 	if(in !=NULL)
@@ -44,11 +45,12 @@ int main(int argc, char *argv[])
 	int p=0;
 	FILE* fin =stdin;
 	int m=0;
+	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	assert(ispow2(size));
-	while((c =getopt(argc, argv, "pf:m"))!=-1){
+	while((c =getopt(argc, argv, "pf:mh"))!=-1){
 		switch(c){
 			case 'p':
 				p=1;
@@ -61,6 +63,9 @@ int main(int argc, char *argv[])
 				break;
 			case 'm':
 				m=1;
+				break;
+			case 'h':
+				h=1;
 				break;	
 			default:
 				break;
@@ -84,7 +89,7 @@ int main(int argc, char *argv[])
 		MPI_Recv(&len, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
 	}
 	MPI_Barrier(MPI_COMM_WORLD);
-	if(len <=0){
+	if(rank ==0  && len <=0){
 		(void)fprintf(stderr,"something went wrong!...len: %d - Rank: %d\n", len, rank);
 		
 	return 1;
@@ -95,7 +100,7 @@ int main(int argc, char *argv[])
 	
 
 	if(in ==NULL || out ==NULL){
-		fprintf(stderr, "ERROR MALLOCING!!\n");
+		fprintf(stderr, "ERROR MALLOCING!!rank = %d\n", rank);
 		return 1;
 	}
 	
@@ -122,7 +127,7 @@ int main(int argc, char *argv[])
 
 	MPI_Barrier(MPI_COMM_WORLD);
 
-	if(counter != len){
+	if(rank ==0 && counter != len){
 		(void)fprintf(stderr, "wrong number amount in stream\n");
 		return 1;
 	}
@@ -160,6 +165,7 @@ int main(int argc, char *argv[])
 	if(rank == 0){
 		(void)printf("According to MPI time fft took %e seconds\n",tmeasure );
 	}
+	
 	if(m)
 		merge(len);	
 
@@ -169,8 +175,10 @@ int main(int argc, char *argv[])
     	print_comp(in, out,len);
 	}
 	MPI_Barrier(MPI_COMM_WORLD);
-	fclose(fin);	
+	fclose(fin);
+		
 	MPI_Finalize();
+	//printf("test %d \n", rank);
 	return 0;
 }
 
@@ -238,22 +246,68 @@ void sprayData(int i, int len){
 		
 }
 
+void help(int len, int i){
+	MPI_Status status;
+	if(size > i/2 && len/i >= size){
+		int k = rank % (i/2);
+		int tbd=(len/i)/(size/(i/2));
+		int t = rank / (i/2);
+
+		if(rank < i/2){
+			for(int r=rank+i/2;r < size;r+=i/2)
+				MPI_Send(out, len, MPI_DOUBLE_COMPLEX,
+						r, 0,MPI_COMM_WORLD);
+		}else{
+			MPI_Recv(out, len, MPI_DOUBLE_COMPLEX,
+			 			k, 0, MPI_COMM_WORLD, &status);
+		}
+		
+		double complex omega = cexp(-2*k*PI*I/i);
+		for(int j = t*tbd;j < (t+1) * tbd; j++){
+			double complex twiddle = omega * out[j*i + k + i/2];
+            out[j*i + k + i/2] = out[j*i + k] - twiddle;
+            out[j*i + k] = out[j*i + k] + twiddle;
+		}	
+		if(rank < i/2){
+			for(int r=rank+i/2;r < size;r+=i/2){
+				MPI_Recv(out+i*(r/(i/2))*tbd+k, len-i*(r/(i/2))*tbd-k, MPI_DOUBLE_COMPLEX,
+			 			r, rank, MPI_COMM_WORLD, &status);
+			}
+		}else{
+			MPI_Send(out+i*t*tbd+k, len-i*t*tbd-k, MPI_DOUBLE_COMPLEX,
+					k, k,MPI_COMM_WORLD);
+		}
+	}else{
+		for(int k =rank; k < i/2; k+=size) {
+			double complex omega = cexp(-2*k*PI*I/i);
+			for(int j = 0;j < len/i; j++){
+				double complex twiddle = omega * out[j*i + k + i/2];
+            	out[j*i + k + i/2] = out[j*i + k] - twiddle;
+    	        out[j*i + k] = out[j*i + k] + twiddle;
+			}
+		}
+	}
+}
+
 void fft(int len)
 {
     /*Fill the output array in bit reversed order, rest of fft can be done inplace*/
     for(int i=0; i<len; i++) {
         out[reverse(i,lg(len))] = in[i];
     }
+	
   	for(int i = 2; i <= len; i *= 2)  {
 		//(void)printf("Ich %d berechne index: ",rank);
-        for(int k =rank; k < i/2; k+=size) {
-	        double complex omega = cexp(-2*k*PI*I/i);
+		if(h)help(len, i);else{
+		for(int k=rank;k < i/2;k+=size){
+	       	double complex omega = cexp(-2*k*PI*I/i);
             for(int j = 0;j < len/i ;j++) {
                 double complex twiddle = omega * out[j*i + k + i/2];
                 out[j*i + k + i/2] = out[j*i + k] - twiddle;
                 out[j*i + k] = out[j*i + k] + twiddle;
             }
 		}
+	}
 		if(i <= size){
 			//MPI_Barrier(MPI_COMM_WORLD);
 			sprayData(i, len);
