9bacbbed66181e68c135c3b347f909678c8b9049
Michael Reitgruber <reitgruber.michael@gmail.com>
2016-01-19 23:36:53 +0100

    modified benchmark, dft benchmark values

--- benchmark.sh
+++ benchmark.sh
@@ -3,16 +3,15 @@ FFT=$1
 START_EXP=$2
 END_EXP=$3
 ITERATIONS=$4
+OUTPUT=$5
 
 echo $FFT >> benchmark_res
 for ((exp = $START_EXP; exp <= END_EXP; exp++))
   do
-    echo e$exp >> benchmark_res
     for ((i = 0; i < $ITERATIONS; i++))
     do
       res[i]="$(./num_gen -e $exp --rectangle | $FFT -b)"
-      echo ${res[i]}
     done
     sorted=($(printf '%s\n' "${res[@]}"|sort))
-    echo ${sorted[$ITERATIONS/2]} >> benchmark_res
+    echo "$exp;${sorted[$ITERATIONS/2]}" >> $OUTPUT
   done
