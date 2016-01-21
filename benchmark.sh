435d9e745dc0ee571bddf0008d0b43b6a763966e
Michael Reitgruber <reitgruber.michael@gmail.com>
2016-01-21 16:22:22 +0100

    fixed benchmark script  :'D

--- benchmark.sh
+++ benchmark.sh
@@ -9,7 +9,7 @@ echo $FFT >> benchmark_res
 for ((exp = $START_EXP; exp <= END_EXP; exp++))
   do
     res="$(./num_gen -e $exp --rectangle | $FFT -b $ITERATIONS)"
-    sorted=($(printf '%s\n' "${res[@]}"|sort))
+    sorted=($(printf '%s\n' "${res[@]}"|sort -n))
     echo "$exp;${sorted[$ITERATIONS/2]}" >> $OUTPUT
   done
   for ((i = 0; i < $ITERATIONS; i++))
